bcf0bd0c19c49db8c082ddc8ec25925b73e1f620
Mac-01 <juan-so174@hotmail.com>
2018-12-05 11:35:50 -0500

    cambio de orden

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -13,7 +13,7 @@ class ProductController extends BaseController
      */
     public function index()
     {
-        $products = Product::all();
+        $products = Product::all()->sortByDesc("id");
         return $this->sendResponse($products->toArray(), 'Products retrieved successfully.');
     }
     /**
